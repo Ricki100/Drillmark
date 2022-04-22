c67de8490db2ddea0df4311e8b37db9314a4087a
Ricardo Chitagu <ricardoprintworks@gmail.com>
2022-04-22 16:47:56 +0200

    tabs

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+$('.vbelt').click (function(){
+  
+  $('.detail-text').toggle(500).css('display','inline-block');
+ 
+  $('.detail-img').toggle(500).css('display','inline-block');
+})
+
+
+
+
+
+
+
+
+
+
+})
